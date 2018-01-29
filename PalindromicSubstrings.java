e1ec5d0b3c4d2cc87b02e6836ee2aaef4da2e4a9
lin0312 <34930993+lin0312@users.noreply.github.com>
2018-01-29 16:48:08 -0500

    String palindr

---
+++ PalindromicSubstrings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSubstrings(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) {
+            count += isPalindrome(s, i, i);
+            count += isPalindrome(s, i, i + 1);
+        }
+        return count;
+    }
+    private int isPalindrome(String s, int l, int r) {
+        int count = 0;
+        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            count++;
+            l--;
+            r++;
+        }
+        return count;
+    }
+}
+//O(n)
